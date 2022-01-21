52d5d4259ac2d43a11f0722ac787d1c52d8e4ff0
moggishere <aryyoon@gmail.com>
2022-01-21 09:48:19 -0300

    adicionado basico

---
+++ blog-pessoal/src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return(
+        <>
+            <h1 className='title'>Home</h1>
+            <p>oi</p>
+            <img src="https://i.imgur.com/H88yIo2.png" alt="imagem de startup" className='imagem' />
+        </>
+    );
+}
+
+export default Home;
