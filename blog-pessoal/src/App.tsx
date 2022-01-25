1ef346d324d2883f5de1c86b767d4fbf4f0a150f
moggishere <aryyoon@gmail.com>
2022-01-24 22:08:21 -0300

    adicionado elementos estaticos e dinamicos

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/statics/navbar/Navbar';
+import Footer from './components/statics/footer/Footer';
 import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import './App.css';
+
 
 function App() {
   return (
-    <Home />
+    <Router>
+      <Navbar />
+      <Switch>
+        <div style={{ minHeight: '100vh' }}>
+
+          <Route exact path='/'>
+            <Login />
+          </Route>
+
+          <Route path='/login'>
+            <Login />
+          </Route>
+
+          <Route path='/home'>
+            <Home />
+          </Route>
+          
+        </div>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
